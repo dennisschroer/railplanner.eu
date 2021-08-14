a3d6a943f2f4e04498c713a39aba7b1c05628a5c
Dennis Schroer <dev@dennisschroer.nl>
2021-08-14 21:51:26 +0200

    Add basic spring-boot project

---
+++ src/main/java/eu/railplanner/railplanner/RailplannerApplication.java
@@ -0,0 +1,13 @@
+package eu.railplanner.railplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RailplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RailplannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/eu/railplanner/railplanner/RailplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.railplanner.railplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RailplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
