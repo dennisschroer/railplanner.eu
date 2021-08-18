b9c6bf4281b8936e656093fbe97f8a7b5b7254ed
Dennis Schroer <dev@dennisschroer.nl>
2021-08-18 12:45:24 +0200

    Parse IFF stations

--- src/main/java/eu/railplanner/railplanner/external/iff/model/IFF.java
+++ src/main/java/eu/railplanner/railplanner/external/iff/model/IFF.java
@@ -1,27 +1,13 @@
 package eu.railplanner.railplanner.external.iff.model;
 
-import eu.railplanner.railplanner.external.iff.parser.FootnoteParser;
-import eu.railplanner.railplanner.external.iff.parser.IFFParser;
-import eu.railplanner.railplanner.external.iff.parser.TimetableParser;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.util.Assert;
 
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayDeque;
-import java.util.List;
-import java.util.Queue;
 
 @Data
 @RequiredArgsConstructor
 public class IFF {
-    public static final Charset IFF_CHARSET = StandardCharsets.ISO_8859_1;
-    public static final String LINE_TERMINATION = "\n\r";
-
     private final Path path;
 
     private Delivery delivery;
@@ -29,4 +15,6 @@ public class IFF {
     private Timetable timetable;
 
     private Footnote footnote;
+
+    private Stations stations;
 }
--- src/main/java/eu/railplanner/railplanner/external/iff/parser/IFFParser.java
+++ src/main/java/eu/railplanner/railplanner/external/iff/parser/IFFParser.java
@@ -7,6 +7,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.util.Assert;
 
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDate;
@@ -17,12 +19,17 @@ import java.util.Queue;
 
 @RequiredArgsConstructor
 public class IFFParser {
+    public static final Charset IFF_CHARSET = StandardCharsets.ISO_8859_1;
+
+    public static final String LINE_TERMINATION = "\n\r";
+
     public static final DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("ddMMyyyy");
 
     public static final DateTimeFormatter TIME_FORMATTER = DateTimeFormatter.ofPattern("HHmm");
 
     public static final String DELIVERY_FILE = "delivery.dat";
     public static final String FOOTNOTE_FILE = "footnote.dat";
+    public static final String STATIONS_FILE = "stations.dat";
     public static final String TIMETABLE_FILE = "timetbls.dat";
 
     public IFF load(Path path) throws IOException {
@@ -30,11 +37,12 @@ public class IFFParser {
         loadDelivery(iff);
         loadTimetable(iff);
         loadFootnote(iff);
+        loadStations(iff);
         return iff;
     }
 
     private void loadDelivery(IFF iff) throws IOException {
-        List<String> lines = Files.readAllLines(iff.getPath().resolve(DELIVERY_FILE));
+        List<String> lines = Files.readAllLines(iff.getPath().resolve(DELIVERY_FILE), IFF_CHARSET);
         Assert.isTrue(lines.size() == 1);
 
         iff.setDelivery(new Delivery());
@@ -42,7 +50,7 @@ public class IFFParser {
     }
 
     private void loadTimetable(IFF iff) throws IOException {
-        Queue<String> lines = new ArrayDeque<>(Files.readAllLines(iff.getPath().resolve(TIMETABLE_FILE)));
+        Queue<String> lines = new ArrayDeque<>(Files.readAllLines(iff.getPath().resolve(TIMETABLE_FILE), IFF_CHARSET));
         Assert.isTrue(lines.size() >= 1);
 
         TimetableParser parser = new TimetableParser();
@@ -50,11 +58,19 @@ public class IFFParser {
     }
 
     private void loadFootnote(IFF iff) throws IOException {
-        Queue<String> lines = new ArrayDeque<>(Files.readAllLines(iff.getPath().resolve(FOOTNOTE_FILE)));
+        Queue<String> lines = new ArrayDeque<>(Files.readAllLines(iff.getPath().resolve(FOOTNOTE_FILE), IFF_CHARSET));
         Assert.isTrue(lines.size() >= 1);
 
         FootnoteParser parser = new FootnoteParser();
         iff.setFootnote(parser.parse(lines));
+    }
+
+    private void loadStations(IFF iff) throws IOException {
+        Queue<String> lines = new ArrayDeque<>(Files.readAllLines(iff.getPath().resolve(STATIONS_FILE), IFF_CHARSET));
+        Assert.isTrue(lines.size() >= 1);
+
+        StationsParser parser = new StationsParser();
+        iff.setStations(parser.parse(lines));
     }
 
     public static IdentificationRecord parseIdentificationRecord(String line) {
