b406471c20778ddd7cca99bc151980e18f72defd
Dennis Schroer <dev@dennisschroer.nl>
2021-10-31 23:35:30 +0100

    Layout for journey planning

---
+++ frontend/src/lib/StationSelect.svelte
@@ -0,0 +1,24 @@
+<script >
+    import Select from 'svelte-select';
+
+    const items = [
+        {value: 'chocolate', label: '🍫 Chocolate', group: 'Sweet'},
+        {value: 'pizza', label: '🍕 Pizza', group: 'Savory'},
+        {value: 'cake', label: '🎂 Cake', group: 'Sweet'},
+        {value: 'cookies', label: '🍪 Cookies', group: 'Savory'},
+        {value: 'ice-cream', label: '🍦 Ice Cream', group: 'Sweet'}
+    ];
+
+    export let selected = undefined;
+
+    function handleSelect(event) {
+        selected = event.detail;
+    }
+
+    function handleClear() {
+        selected = undefined;
+    }
+</script>
+
+
+<Select id="food" {items} on:select={handleSelect} on:clear={handleClear}></Select>
--- frontend/src/routes/index.svelte
+++ frontend/src/routes/index.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import StationSelect from "$lib/StationSelect.svelte";
+
+    let startStation = undefined;
+    let destinationStation = undefined;
+    let startTime = new Date();
+</script>
+
+<form class="row g-3">
+    <div class="col-12 col-md-4">
+        <label class="form-label">From</label>
+        <StationSelect bind:selected={startStation}/>
+    </div>
+
+    <div class="col-12 col-md-4">
+        <label class="form-label">To</label>
+        <StationSelect bind:selected={destinationStation}/>
+    </div>
+
+    <div class="col-12 col-md-4">
+        <label class="form-label">Departure</label>
+        <div>{startTime}</div>
+    </div>
+
+    <button type="submit" class="btn btn-primary">Search journey</button>
+</form>
